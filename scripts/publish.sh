e63a09e965a92261703503ad0c56b1f0e8362fdc
Kyryl R <kyryl.ryabov@gmail.com>
2023-09-04 18:43:47 +0300

    Updated package.json scripts. Deleted redundant scripts folder

--- scripts/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-cp -r ./package.json ./README.md ./LICENSE ./dist ./src ./publish
-npm publish ./publish/ --access public
-rm -r ./publish/package.json ./publish/README.md ./publish/LICENSE ./publish/dist ./publish/src
