377ea8c7e5b5904937e9e2ef7c6844954dc0e466
Ruslan Kasheparov <ruslankasheparov@gmail.com>
2023-09-06 17:50:50 +0200

    Added axios to dependencies/fix husky

--- scripts/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-rm -r ./publish/README.md ./publish/LICENSE ./publish/dist ./publish/src
-rm -r ./sample-project/artifacts ./sample-project/cache
-rm -r ./dist
--- scripts/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-cp -r ./README.md ./LICENSE ./dist ./src ./publish
-npm publish ./publish/ --access public
-rm -r ./publish/README.md ./publish/LICENSE ./publish/dist ./publish/src
