50b296b9ca235cf2a0cdf098b8a096b80a4aeeb8
Kyryl R <kyryl.ryabov@gmail.com>
2023-09-08 19:29:10 +0300

    Used only one deploy

--- test/fixture-projects/hardhat-project-minimal-typechain-ethers/migrates/1_deploy.ts
+++ test/fixture-projects/hardhat-project-minimal-typechain-ethers/migrates/1_deploy.ts
@@ -1,17 +1,11 @@
 import { Deployer } from "../../../../src/deployer/Deployer";
 
-import { ContractWithConstructorArguments } from "../typechain-types";
+import { ContractWithConstructorArguments__factory } from "../typechain-types";
 
 export = async (deployer: Deployer) => {
-  let contract: ContractWithConstructorArguments;
-
-  contract = await deployer.deploy("ContractWithConstructorArguments", ["hello"], {
+  let contract = await deployer.deploy(ContractWithConstructorArguments__factory, ["hello"], {
     gasLimit: 1000000,
   });
 
-  contract = await deployer.deploy2<ContractWithConstructorArguments>([contract], {
-    gasLimit: 1000000,
-  });
-
-  contract.name();
+  await contract.name();
 };
--- test/fixture-projects/hardhat-project-minimal-typechain-truffle/migrates/1_deploy.ts
+++ test/fixture-projects/hardhat-project-minimal-typechain-truffle/migrates/1_deploy.ts
@@ -1,16 +1,11 @@
 import { Deployer } from "../../../../src/deployer/Deployer";
-import { ContractWithConstructorArgumentsContract, ContractWithConstructorArgumentsInstance } from "../typechain-types";
+
+const ContractWithConstructorArguments = artifacts.require("ContractWithConstructorArguments");
 
 export = async (deployer: Deployer) => {
-  let contract: ContractWithConstructorArgumentsInstance;
-
-  contract = await deployer.deploy("ContractWithConstructorArguments", ["hello"], {
+  let contract = await deployer.deploy(ContractWithConstructorArguments, ["hello"], {
     gasLimit: 1000000,
   });
 
-  contract = await deployer.deploy2<ContractWithConstructorArgumentsContract>(["hello"], {
-    gasLimit: 1000000,
-  });
-
-  contract.name();
+  await contract.name();
 };
