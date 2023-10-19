0024afa128f34ae4c936f3c8724c082a5254c216
Kyryl R <kyryl.ryabov@gmail.com>
2023-10-20 00:17:02 +0300

    Resolved types -- cleaned up

--- src/deployer/Deployer.ts
+++ src/deployer/Deployer.ts
@@ -2,12 +2,20 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 import { DeployerCore } from "./DeployerCore";
 
-import { catchError, getSignerHelper, resolveAdapter } from "../utils";
+import { catchError, getSignerHelper, isEthersFactory, isPureFactory, isTruffleFactory } from "../utils";
+
+import { MigrateError } from "../errors";
+
+import { Adapter } from "./adapters/Adapter";
+import { PureAdapter } from "./adapters/PureAdapter";
+import { EthersAdapter } from "./adapters/EthersAdapter";
+import { TruffleAdapter } from "./adapters/TruffleAdapter";
+
+import { OverridesAndLibs } from "../types/deployer";
+import { Instance, TypedArgs } from "../types/adapter";
 
 import { ArtifactProcessor } from "../tools/storage/ArtifactProcessor";
 import { TransactionProcessor } from "../tools/storage/TransactionProcessor";
-import { Instance, ProxyTypedArgs, TypedArgs } from "../types/adapter";
-import { Args, OverridesAndLibs } from "../types/deployer";
 
 @catchError
 export class Deployer {
@@ -16,12 +24,12 @@ export class Deployer {
     private _core = new DeployerCore(_hre),
   ) {}
 
-  public async deploy<A, I>(
-    contract: Instance<A, I>,
-    args: TypedArgs<A> = [] as any,
+  public async deploy<T, A = T, I = any>(
+    contract: Instance<A, I> | (T extends Truffle.Contract<I> ? T : never),
+    args: TypedArgs<A>,
     parameters: OverridesAndLibs = {},
   ): Promise<I> {
-    const adapter = resolveAdapter(this._hre, contract);
+    const adapter = this.resolveAdapter(this._hre, contract);
 
     const deploymentParams = await adapter.getContractDeployParams(contract);
 
@@ -30,57 +38,8 @@ export class Deployer {
     return adapter.toInstance(contract, contractAddress, await getSignerHelper(this._hre, parameters.from));
   }
 
-  public async deployProxy<A, I, B, J>(
-    contract: Instance<A, I>,
-    initArgs: Args = [] as any,
-    parameters: OverridesAndLibs = {},
-    opts: {
-      kind: Instance<B, J>;
-      args: ProxyTypedArgs<B>;
-      initializer?: string | false;
-      txOverrides?: OverridesAndLibs;
-      constructorArgs?: TypedArgs<A>;
-    },
-  ): Promise<I> {
-    const adapter = resolveAdapter(this._hre, contract);
-
-    const contractImplementationAddress = await this._core.deploy(
-      await adapter.getContractDeployParams(contract),
-      opts.constructorArgs || [],
-      parameters,
-    );
-
-    let proxyAddress: string = "";
-    if (this._isUUPSProxy(contract)) {
-      const proxyArgs = [contractImplementationAddress, ...opts.args];
-
-      proxyAddress = await this._core.deploy(
-        await adapter.getContractDeployParams(opts.kind),
-        proxyArgs,
-        opts.txOverrides || {},
-      );
-    }
-
-    const contractInstance = adapter.toInstance(
-      contract,
-      proxyAddress,
-      await getSignerHelper(this._hre, parameters.from),
-    );
-
-    if (opts.initializer) {
-      await (contractInstance as any)[opts.initializer](...initArgs);
-    }
-
-    TransactionProcessor.saveDeploymentByName(
-      ArtifactProcessor.getContractName((await adapter.getContractDeployParams(contract)).bytecode),
-      proxyAddress,
-    );
-
-    return contractInstance;
-  }
-
   public async deployed<A, I>(contract: Instance<A, I>): Promise<I> {
-    const adapter = resolveAdapter(this._hre, contract);
+    const adapter = this.resolveAdapter(this._hre, contract);
 
     const contractName = ArtifactProcessor.getContractName((await adapter.getContractDeployParams(contract)).bytecode);
 
@@ -90,11 +49,22 @@ export class Deployer {
   }
 
   public async link<A, I>(library: any, instance: Instance<A, I>): Promise<void> {
-    await resolveAdapter(this._hre, instance).link(library, instance);
+    await this.resolveAdapter(this._hre, instance).link(library, instance);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  private _isUUPSProxy(instance: any): boolean {
-    return true;
+  public resolveAdapter<A, I>(hre: HardhatRuntimeEnvironment, contract: Instance<A, I>): Adapter {
+    if (isEthersFactory(contract)) {
+      return new EthersAdapter(hre);
+    }
+
+    if (isTruffleFactory(contract)) {
+      return new TruffleAdapter(hre);
+    }
+
+    if (isPureFactory(contract)) {
+      return new PureAdapter(hre);
+    }
+
+    throw new MigrateError("Unknown Contract Factory Type");
   }
 }
