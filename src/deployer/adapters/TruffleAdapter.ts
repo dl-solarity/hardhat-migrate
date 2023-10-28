cf86aa7c9fe362228b359ed5a592329ffb8ab7f7
Kyryl R <kyryl.ryabov@gmail.com>
2023-10-28 13:49:37 +0300

    Returned unused parameter

--- src/deployer/adapters/TruffleAdapter.ts
+++ src/deployer/adapters/TruffleAdapter.ts
@@ -42,7 +42,8 @@ export class TruffleAdapter extends Adapter {
     );
   }
 
-  public async toInstance<I>(instance: TruffleFactory<I>, address: string): Promise<I> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async toInstance<I>(instance: TruffleFactory<I>, address: string, _: OverridesAndMisc): Promise<I> {
     const contract = this._hre.artifacts.require(instance.contractName!);
 
     await this._overrideConnectMethod(instance);
