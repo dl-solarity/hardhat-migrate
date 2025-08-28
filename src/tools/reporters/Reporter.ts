a9dffb0ecbc5c2460eac134f8b56c029c0a4b930
Kyryl R <kyryl.ryabov@gmail.com>
2025-08-28 17:14:59 +0300

    Fixed verification reporting with blockscout

--- src/tools/reporters/Reporter.ts
+++ src/tools/reporters/Reporter.ts
@@ -15,8 +15,6 @@ import { MigrateConfig } from "../../types/migrations";
 import { ChainRecord, CustomChainRecord, predefinedChains } from "../../types/verifier";
 import { ContractFieldsToSave, MigrationMetadata, TransactionFieldsToSave } from "../../types/tools";
 
-// TODO: fix verification reporting with blockscout
-
 /**
  * Global error handling for network-related issues is conducted within the NetworkManager class
  */
@@ -503,13 +501,18 @@ class BaseReporter {
   }
 
   private _getInfoFromHardhatConfig(chainId: number): CustomChainRecord | undefined {
-    let customChains: CustomChainRecord[] = [];
+    const cfg = this._hre.config as any;
 
-    if ((this._hre.config as any).etherscan && (this._hre.config as any).etherscan.customChains) {
-      customChains = (this._hre.config as any).etherscan.customChains;
-    }
+    const etherscanChains: CustomChainRecord[] = cfg.etherscan?.customChains ?? [];
+    const blockscoutChains: CustomChainRecord[] = cfg.blockscout?.customChains ?? [];
 
-    return customChains.find((chain) => chain.chainId === chainId);
+    const merged: CustomChainRecord[] = [...blockscoutChains, ...etherscanChains];
+
+    const deduped = merged.filter(
+      (chain, index, self) => index === self.findIndex((c) => c.chainId === chain.chainId),
+    );
+
+    return deduped.find((chain) => chain.chainId === chainId);
   }
 
   private async _tryGetAllRecords(): Promise<ChainRecord[]> {
